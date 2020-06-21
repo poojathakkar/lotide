f30bed7a99bb72e591502f79ccb9bc30d86942cf
Pooja Thakkar <poojapthakkar@gmail.com>
2020-06-21 18:18:43 +0000

    Test file for assertArraysEqual

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+// TEST CODE
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); 
+assertArraysEqual([1, 2, 3], [1,2]); 
+
+
