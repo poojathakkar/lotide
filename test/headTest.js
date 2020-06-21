6321f02765311bae9173743a022d9c1e324e4774
Pooja Thakkar <poojapthakkar@gmail.com>
2020-06-21 15:25:47 +0000

    Test file for head

---
+++ test/headTest.js
@@ -0,0 +1,8 @@
+// TEST CODE
+const assertEqual = require('../assertEqual');
+const head = require('../head');
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 5);
+assertEqual(head(["Hello"]), "Hello");
