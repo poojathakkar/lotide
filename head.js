6933e032fc2798ee9a299dd015e0ba4e735a908a
Pooja Thakkar <poojapthakkar@gmail.com>
2020-06-21 15:21:24 +0000

    removed Tests

--- head.js
+++ head.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(` ✔ ✔ ✔ Assertion Passed: ${actual} === ${expected}.`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}.`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   
@@ -13,8 +6,5 @@ const head = function(array) {
   return element;
 };
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
-assertEqual(head(["Hello"]), "Hello");
+module.exports = assertEqual;
+module.exports = head;
