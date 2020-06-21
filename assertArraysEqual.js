031956d23f99fb5d6804371575d4d2ca5352b4c2
Pooja Thakkar <poojapthakkar@gmail.com>
2020-06-21 18:17:08 +0000

    remove tests in eqArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,13 @@
 // TEST CODE
-const eqArrays = function(array1, array2) {
-  let value = false;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      value = false;
-    } else {
-      value = true;
-    }
-    
-    } 
-  return value;    
-};
+const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function(array1, array2) {
+const assertArraysEqual = function(actual, expected) {
   
-  console.log(eqArrays(array1,array2));
+  if(eqArrays(actual,expected)) {
+    console.log(` ✔ ✔ ✔ Assertion Passed: ${[actual]} === ${[expected]}.`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${[actual]} !== ${[expected]}.`);
+  }
 };
 
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); 
+module.exports = assertArraysEqual;
